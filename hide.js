320ed1a5453b5511cb28a4e06a5d0d632b995574
Benedikt Sauer <sauer.benedikt@outlook.de>
2023-07-18 22:33:41 +0200

    feat: make add on dynamcially search for parent DOM

--- hide.js
+++ hide.js
@@ -1,32 +1,84 @@
-const streamers = [
-    'scurrows',
-]
-
-console.info('Twitch Streamer Hider loading...');
+const suggestedChannelIdentifierLeftBar = "ScTransitionBase-sc";
+const suggestedChannelIdentifierCenterBig = "ScTransformWrapper-sc";
+const suggestedChannelIdentifierFeaturedChannels =
+  "featured-content-carousel__item-container";
 
 async function init() {
-    console.info('Twitch Streamer Hider loaded');
-    searchStreamer();
+  console.info("Twitch Streamer Hider loaded");
+
+  const defaultStreamers = await fetchStreamer();
+  const customStreamers = await fetchCustomStreamer();
+
+  const streamers = [...defaultStreamers, ...customStreamers];
+
+  setInterval(() => {
+    if (isOnHideStreamerPage(streamers)) {
+      window.location.href = "https://www.twitch.tv/";
+    } else {
+      searchStreamer(streamers);
+    }
+  }, 2000);
 }
 
-function searchStreamer() {
-    const bodyDOM = document.querySelector('html');
+async function fetchStreamer() {
+  const streamersJSONURL = chrome.runtime.getURL("streamer.json");
+  const response = await fetch(streamersJSONURL);
+  const streamers = await response.json();
 
-    streamers.forEach((streamer) => {
-        if (bodyDOM.innerHTML.indexOf(streamer) > -1) {
-            console.log(`Streamer ${streamer} is live!`)
-            removeStreamer(streamer);
-        }
-    });
+  return streamers;
+}
+
+async function fetchCustomStreamer() {
+  try {
+    const streamersJSONURL = chrome.runtime.getURL("streamer.custom.json");
+    const response = await fetch(streamersJSONURL);
+    const streamers = await response.json();
+
+    return streamers;
+  } catch (e) {
+    // No custom streamers
+    return [];
+  }
+}
+
+function searchStreamer(streamers) {
+  const bodyDOM = document.querySelector("html");
+
+  streamers.forEach((streamer) => {
+    if (bodyDOM.innerHTML.indexOf(streamer) > -1) {
+      try {
+        removeStreamer(streamer);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+}
+
+function isOnHideStreamerPage(streamers) {
+  const url = window.location.href;
+
+  return streamers.some((streamer) => url.includes(streamer));
 }
 
 function removeStreamer(streamer) {
-    const streamerDOM = document.querySelector(`[title="${streamer}"]`);
-    streamerDOM.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.
-    style.display = 'none';
+  const streamerDOM = document.querySelectorAll(`[href="/${streamer}"]`);
+  streamerDOM.forEach((element) => {
+    let parent = element.parentElement;
+    let counter = 0;
+    while (counter < 20) {
+      if (
+        parent.className.includes(suggestedChannelIdentifierLeftBar) ||
+        parent.className.includes(suggestedChannelIdentifierCenterBig) ||
+        parent.className.includes(suggestedChannelIdentifierFeaturedChannels)
+      ) {
+        parent.remove();
+        break;
+      }
+      parent = parent.parentElement;
+      counter++;
+    }
+  });
 }
 
-
-setTimeout(() => {
-    init().catch((e) => logError(e));
-}, 3000);
+init();
